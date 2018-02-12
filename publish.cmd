64dcb860adfc495a925f46fefe857d86e8c94ae3
Joffre Bernal <joffre.bernal@live.com>
2018-02-12 00:19:17 -0500

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=tamebot-Web-Deploy.pubxml -p:Password=MxJCjhgd1PdT9NEgKsKeCQ7dnMJ9J6Hrvb8tK5tSoWsMPfjQou7yEkKBGAez
+
